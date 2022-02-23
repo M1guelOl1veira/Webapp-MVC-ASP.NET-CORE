37d895b343f2641434a688f2a43e6d47de0aaa9c
M1guelOl1veira <miguel.ao1086@gmail.com>
2022-02-23 16:01:48 -0300

    Estudo - Desenvolvimento da aplicação web

---
+++ 1. Transformando a aplicação em um servidor web/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var _repo = new LivroRepositorioCSV();
+
+            //criar objeto para hospedar os pedidos web.
+            IWebHost host = new WebHostBuilder() //WebHostBuilder constrói um hospedeiro web, mas não implementa a interface.
+                .UseKestrel() //Cria um web host que usa a implentação do Kestrel.
+                .UseStartup<Startup>() //Dizendo que o código de configuração ficará na classe Startup.
+                .Build(); // O Build implementa essa interface.
+                
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer); 
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+          
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ 1. Transformando a aplicação em um servidor web/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            //IApplicationBuilder Constrói o pipeline(fluxo de requisição resposta) de requisição para a minha aplicação.
+            //Criar uma coleção de rotas com o objeto do tipo RouterBuilder, que recebe a aplicação como parâmetro.
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLidos);
+            var rotas = builder.Build();
+            app.UseRouter(rotas);
+
+            //  app.Run(Roteamento);
+        }
+        //HttpContext contem toda informação da requisição enviada.
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>//Tipo da chave e tipo do valor respectivamente. RequestDelegate é um tipo que permite passar um método como argumento.
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
+            };
+
+            //context.Request.Path É o caminho da requisição.
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
+            }
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho não encontrado!");
+ 
+        }
+        public Task LivrosParaLer(HttpContext context)
+        {
+            //RequestDelegate tem como retorno o tipo Task, estudar paralelismo para mais informações.
+            //Código executado quando uma requisição chegar.
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            //_repo.ParaLer.ToString(); Transforma a lista ParaLer em uma string.
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+    }
+}
